136cba21d1c161d6bca10d3bac712d8a1259fdeb
Richard Musime <richardmusime6@gmail.com>
2025-01-10 09:00:54 +0200

    added the logic for cart view

--- webapp/controller/Cart.controller.js
+++ webapp/controller/Cart.controller.js
@@ -2,17 +2,44 @@ sap.ui.define([
     "./BaseController", 
     "sap/m/MessageBox",
     "sap/ui/model/json/JSONModel",
-    "sap/m/TabContainerItem"
-], function (BaseController, MessageBox, JSONModel, TabContainerItem) {
+    "sap/ui/model/odata/v4/ODataModel"
+], function (BaseController, MessageBox, JSONModel, ODataModel) {
     "use strict";
     return BaseController.extend("mercedsapp.controller.Cart", {
         onInit: function() {
-           
-        },
-        handleDelete(){
+            const oModel = new ODataModel({
+                serviceUrl: "http://localhost:4000/odata/",
+                synchronizationMode: "None"
+            });
             
-        }
+            this.getView().setModel(oModel);
+            
+            const oCartModel = new JSONModel({
+                cartItems: []
+            });
+            this.getView().setModel(oCartModel, "cart");
+            
+            this._loadCartItems();
+        },
 
-        
-    });
-});
+        _loadCartItems: async function() {
+            try {
+                const oModel = this.getView().getModel();
+                
+                const oContext = await oModel.bindList("/getCartItems").requestContexts();
+                
+                const aCartItems = oContext.map(oContext => oContext.getObject());
+                console.log(aCartItems);
+
+                const oCartModel = this.getView().getModel("cart");
+                oCartModel.setProperty("/getCartItems", aCartItems);
+                
+            } catch (error) {
+                MessageBox.error("Failed to load cart items");
+                console.error(error);
+            }
+        },
+			handleDelete: function () {},
+		});
+	}
+);
